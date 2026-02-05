827b584025a52ceeb7f90def128e68e2a4da4d83
Peyou Tessa <PeyouTessa@MacBook-Pro-2.local>
2026-02-05 11:44:30 +0100

    init node-action

---
+++ test/sum.test.js
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest'     
+import { sum } from '..src/sum'
+describe('sum function', () => {
+  it('should return the sum of two numbers', () => {
+    expect(sum(2, 3)).toBe(5)
+  })
